5cdb9e9cfc7d390f21efdf88fbccc918f9638569
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-27 10:26:49 +0200

    rename array_size macro + update usage

--- src/main.c
+++ src/main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-const char *help[] =
-{
+#define ARRAY_SIZE(x)       (sizeof(x) / sizeof((x)[0]))
+
+const char *help[] = {
+	"-h, --help				Display this information.",
 	"-c, --compile-command			command to compile",
 	"-b, --binary-name			binary name to execute",
-	"-s, --source-location PATH(,PATH2...)	sources location to watch,\n\
-						If PATH is a directory,	\n\
-						all files inside it will \n\
+	"-s, --source-location PATH(,PATH2...)	If PATH is a directory,\n\
+						all files inside it will\n\
 						be concerned."
 };
-
-#define SIZEOF(x)       (sizeof(x) / sizeof(x[0]))
 
 int usage(char **argv)
 {
 	fprintf(stderr, "%s [OPTIONS]\n\n", argv[0]);
 	fprintf(stderr, "OPTIONS:\n");
-	for (size_t i = 0; i < SIZEOF(help); i++)
-		fprintf(stderr, "\t%s\n", help[i]);
+	for (size_t i = 0; i < ARRAY_SIZE(help); i++)
+		fprintf(stderr, "\t%s\n\n", help[i]);
 	return 0;
 }
 
