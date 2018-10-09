84d5141ad40f2ded2613cf08eacea32af9cf5607
Pacôme Françon <pacome.francon@epitech.eu>
2018-10-09 22:40:27 +0200

    WATCH_MASK for inotify_watch() -> only IN_CLOSE_WRITE
    The "IN_CLOSE_WRITE" event corresponds to the saving of a file, so we're only waiting for him.

--- include/livereload.h
+++ include/livereload.h
@@ -5,8 +5,7 @@
 #define ARRAY_SIZE(x)		(sizeof(x) / sizeof((x)[0]))
 #define destructor(destructor)	__attribute__((cleanup(destructor)))
 
-#define WATCH_MASK		IN_OPEN | IN_CREATE | IN_MODIFY |	\
-				IN_DELETE | IN_CLOSE_WRITE
+#define WATCH_MASK		IN_CLOSE_WRITE
 
 int	usage(char **argv);
 void	add_watched_files(int inotify_fd, char **source_path);
