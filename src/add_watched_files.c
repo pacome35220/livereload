1bf35a553fd2212a3fb939c3314acfcde56eb76f
Pacôme Françon <pacome.francon@epitech.eu>
2019-02-05 13:01:53 +0100

    add include .h

--- src/add_watched_files.c
+++ src/add_watched_files.c
@@ -1,13 +1,14 @@
+#include <unistd.h>
 #include "livereload.h"
 
 void add_watched_files(int inotify_fd, char **source_path)
 {
-	int tmp;
+        int tmp;
 
-	for (size_t i = 0; source_path[i] != NULL; i++) {
-		tmp = inotify_add_watch(inotify_fd, source_path[i], WATCH_MASK);
-		if (tmp == -1)
-			fprintf(stderr, BAD_SOURCE, source_path[i]);
-		close(tmp);
-	}
+        for (size_t i = 0; source_path[i] != NULL; i++) {
+                tmp = inotify_add_watch(inotify_fd, source_path[i], WATCH_MASK);
+                if (tmp == -1)
+                        fprintf(stderr, BAD_SOURCE, source_path[i]);
+                close(tmp);
+        }
 }
